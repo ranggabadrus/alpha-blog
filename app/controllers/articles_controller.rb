fe187563ece679c45bc0b89fa28c79d1e73cdae0
Rangga Badrus <ranggabadrus@gmail.com>
2019-11-06 08:55:35 +0000

    extract redundancy and prepare for PDT

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
   
+  before_action :set_params, only: [:edit, :update, :show, :destroy]
+  
   def index
     @articles = Article.all
   end
@@ -9,11 +11,9 @@ class ArticlesController < ApplicationController
   end
   
   def edit
-    @article = Article.find(params[:id])
   end
   
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "Article was successfully updated"
       redirect_to article_path(@article)
@@ -34,10 +34,19 @@ class ArticlesController < ApplicationController
   end
   
   def show
-    @article = Article.find(params[:id])
+  end
+  
+  def destroy
+    @article.destroy
+    flash[:notice] = "Article was successfully deleted"
+    redirect_to articles_path
   end
   
   private
+  
+    def set_params
+        @article = Article.find(params[:id])
+    end
     def article_params
       params.required(:article).permit(:title, :description)
     end
